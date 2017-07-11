ec449f65ca04d844e7e7d399bf3754315cc7dec0
An-AngryBear <monahajt@gmail.com>
2017-07-11 11:05:44 -0500

    ajax in attractions

---
+++ javascripts/attractions.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let $ = require('jquery');
+
+let attractions = {};
+
+attractions.getAttractions = function() {
+	return new Promise(function(resolve, reject) {
+		$.ajax({
+			url: "https://gitbearified.firebaseio.com/attractions"
+		})
+		.done(function(data) {
+			resolve(data);
+		})
+		.fail(function(error) {
+			console.log(error.statusText);
+		});
+	});
+};
