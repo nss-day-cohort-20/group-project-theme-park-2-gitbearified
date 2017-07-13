af445c37035bbc268bd0980f25164fbb2e9628e5
Megan B <brown.m.j@outlook.com>
2017-07-13 13:01:45 -0500

    current footer year

--- javascripts/DOM-manager.js
+++ javascripts/DOM-manager.js
@@ -5,12 +5,7 @@ let $ = require('jquery');
 let DOMmanager = Object.create(null);
 
 DOMmanager.writeToDOM = function (stuffToPrint, whereToPrint) {
-	// console.log("stuff passed in", stuffToPrint);
 	whereToPrint.html(stuffToPrint);
 };
-// DOMmanager.writeToInfoBox = function (stuffToPrint) {
-// 	// console.log("stuff passed in", stuffToPrint);
-// 	$parkInfoDiv.html(stuffToPrint);
-// };
 
 module.exports = DOMmanager;
